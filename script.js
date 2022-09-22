6259a89df3f0ad712a98807654dc30d1cbc1ed02
Glitch (empty-glitch) <none>
2022-09-22 22:14:31 +0000

    👸👴 Checkpoint ./index.html:11113072/14754 ./script.js:11113072/14819 ./style.css:11113072/207

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ document.querySelector("input").onchange = function () {
   if (files.length < 1) return;
   var fr = new FileReader();
   fr.onload = function (e) {
-    var formatted = JSON.stringify(e.target.result);
-    document.body.textContent += formatted;
+    var raw = JSON.stringify(e.target.result).split('"');
+    alert(Date.now())
+    var i=0
+    raw.forEach(function (e) {
+      i++
+      if (e.includes("QWxsb3dPbmx5UG9saWN5")) {
+        alert(e)
+      }    
+    });
+    alert(Date.now())
   };
   fr.readAsText(files.item(0));
 };
