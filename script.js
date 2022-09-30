247d81ad7f0a062ea626ec177cc22d204b9e7d25
Glitch (empty-glitch) <none>
2022-09-30 23:26:50 +0000

    👍🏃 Checkpoint ./index.html:11113072/1356 ./script.js:11113072/12094

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var net;
+var button = document.querySelector("button");
 var pre = document.querySelector("pre");
 pre.textContent =
   "upload your chrome://net-export file that was active while reloading chrome policies";
@@ -13,8 +15,13 @@ document.querySelector("input").onchange = function () {
         e.includes("VHlwZSI6ICJXaUZpIiw") ||
         e.includes("eXBlIjogIldpRmkiLA")
       ) {
-        throw (pre.textContent = atob(e)
+        net = atob(e)
           .replace(/\s\s+/g, " ")
+          .replace(/\\u([\d\w]{4})/gi, function (a, b) {
+            return String.fromCharCode(parseInt(b, 16));
+          });
+        button.removeAttribute("disabled");
+        throw (pre.textContent = net
           .split('"WiFi": {')
           .join("youtu.be/dQw4w9WgXcQ")
           .split(', "AutoConnect"')
@@ -23,13 +30,14 @@ document.querySelector("input").onchange = function () {
           .filter(function (el, index) {
             return index % 2 === 1;
           })
-          .join("\n")
-          .replace(/\\u([\d\w]{4})/gi, function (a, b) {
-            return String.fromCharCode(parseInt(b, 16));
-          }));
+          .join("\n"));
       }
       pre.textContent =
         "no passwords were found; make sure you were importing the right file and have a DeviceOpenNetworkConfiguration in chrome://policy";
+      button.disabled = true;
     });
   };
 };
+function raw() {
+  open().document.write(net);
+}
