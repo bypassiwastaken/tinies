6d4e19aa14f90fb8dddf83ec2fb5e944ea95ef04
Glitch (empty-glitch) <none>
2022-10-19 15:21:25 +0000

    🐝🍶 Checkpoint ./test.js:11113072/2599

--- test.js
+++ test.js
@@ -6,16 +6,30 @@ document.querySelector("input").onchange = function () {
   var fr = new FileReader();
   fr.readAsText(this.files.item(0));
   fr.onload = function (e) {
-    open().document.write(e.target.result);
-    var raw = JSON.stringify(e.target.result).split('\\"');
-    raw.forEach(function (e) {
-      if (
-        e.includes("aGFsZGxnbGRwbGduZ2c") ||
-        e.includes("YWxkbGdsZHBsZ25nZw") ||
-        e.includes("bGRsZ2xkcGxnbmdn")
-      ) {
-        throw (pre.textContent = atob(e));
-      }
-    });
+    open().document.write(
+      e.target.result
+        .replace(
+          "aGFsZGxnbGRwbGduZ2dramFhZmhlbGdpYWdsYWZhbmg",
+          "aGFsZGxnbGRwbGduZ2dramFhZmhlbGdpYWdsYWZhbmh"
+        )
+        .replace(
+          "YWxkbGdsZHBsZ25nZ2tqYWFmaGVsZ2lhZ2xhZmFuaA",
+          "YWxkbGdsZHBsZ25nZ2tqYWFmaGVsZ2lhZ2xhZmFuaB"
+        )
+        .replace(
+          "bGRsZ2xkcGxnbmdna2phYWZoZWxnaWFnbGFmYW5o",
+          "bGRsZ2xkcGxnbmdna2phYWZoZWxnaWFnbGFmYW5p"
+        )
+    );
+    // var raw = JSON.stringify(e.target.result).split('\\"');
+    // raw.forEach(function (e) {
+    //   if (
+    //     e.includes("aGFsZGxnbGRwbGduZ2c") ||
+    //     e.includes("YWxkbGdsZHBsZ25nZw") ||
+    //     e.includes("bGRsZ2xkcGxnbmdn")
+    //   ) {
+    //     throw (pre.textContent = atob(e));
+    //   }
+    // });
   };
 };
