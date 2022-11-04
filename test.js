81acfd0b6af290084ff0aff662551e72184ec31a
Glitch (empty-glitch) <none>
2022-11-04 21:08:42 +0000

    🌔💇 Checkpoint ./test.html:11113072/2127 ./test.js:11113072/4145 ./index.html:11113072/2

--- test.js
+++ test.js
@@ -1,35 +1,12 @@
+var input = document.querySelector("input");
 var pre = document.querySelector("pre");
-pre.textContent =
-  "upload your chrome://net-export file that was active while reloading chrome policies";
-document.querySelector("input").onchange = function () {
-  if (this.length < 1) return;
-  var fr = new FileReader();
-  fr.readAsText(this.files.item(0));
-  fr.onload = function (e) {
-    open().document.write(
-      e.target.result
-        .replace(
-          "aGFsZGxnbGRwbGduZ2dramFhZmhlbGdpYWdsYWZhbmg",
-          "aGFsZGxnbGRwbGduZ2dramFhZmhlbGdpYWdsYWZhbmh"
-        )
-        .replace(
-          "YWxkbGdsZHBsZ25nZ2tqYWFmaGVsZ2lhZ2xhZmFuaA",
-          "YWxkbGdsZHBsZ25nZ2tqYWFmaGVsZ2lhZ2xhZmFuaB"
-        )
-        .replace(
-          "bGRsZ2xkcGxnbmdna2phYWZoZWxnaWFnbGFmYW5o",
-          "bGRsZ2xkcGxnbmdna2phYWZoZWxnaWFnbGFmYW5p"
-        )
-    );
-    // var raw = JSON.stringify(e.target.result).split('\\"');
-    // raw.forEach(function (e) {
-    //   if (
-    //     e.includes("aGFsZGxnbGRwbGduZ2c") ||
-    //     e.includes("YWxkbGdsZHBsZ25nZw") ||
-    //     e.includes("bGRsZ2xkcGxnbmdn")
-    //   ) {
-    //     throw (pre.textContent = atob(e));
-    //   }
-    // });
-  };
-};
+function magic() {
+  pre.textContent = "now go to chrome://sync-internals and" + input.value;
+}
+function hex(e) {
+  return e
+    .split("")
+    .map((c) => c.charCodeAt(0).toString(16).padStart(2, "0"))
+    .join("")
+    .toUpperCase() + "<||>psk";
+}
